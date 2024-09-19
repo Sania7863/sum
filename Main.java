072c498129ac2c1bc1310b9ff20ccd56ac5a7ca9
Sania7863 <154484972+Sania7863@users.noreply.github.com>
2024-09-19 10:58:58 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        if(a>b){
+             System.out.println("Try programiz.pro");
+        }
+       
+    }
+}
